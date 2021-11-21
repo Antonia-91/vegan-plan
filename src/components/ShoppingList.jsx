57bdbfd3b842a9a6c05073073b04e0c68ae0b66c
tovebratt <du@example.com>
2021-11-21 16:56:13 +0100

    fixed warnings shoppinglist component

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -7,28 +7,28 @@ const ShoppingList = () => {
   <p className="text-fat-big-upper">Veckans inköpslista</p>
   <table className="table-shoppinglist">
       <tbody>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Purjolök</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Aubergine</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Ingefära</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">3 st</td><td className="fat">+</td><td>Krossad tomat</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">500 g</td><td className="fat">+</td><td>Spenat</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">250 g</td><td className="fat">+</td><td>Valnötter</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Broccoli</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">5 st</td><td className="fat">+</td><td>Sötpotatis</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Gurkmeja</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Olivolja</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">500 g</td><td className="fat">+</td><td>Mangold</td></tr>
-        <tr><input type="checkbox" className="checked"/><span class="checkmark"></span><td className="fat">-</td><td className="fat">250 g</td><td className="fat">+</td><td>Jordärtskocka</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Purjolök</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Aubergine</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Ingefära</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">3 st</td><td className="fat">+</td><td>Krossad tomat</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">500 g</td><td className="fat">+</td><td>Spenat</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">250 g</td><td className="fat">+</td><td>Valnötter</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Broccoli</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">5 st</td><td className="fat">+</td><td>Sötpotatis</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Gurkmeja</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">1 st</td><td className="fat">+</td><td>Olivolja</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">500 g</td><td className="fat">+</td><td>Mangold</td></tr>
+        <tr><td><input type="checkbox" className="checked"/></td><td className="checkmark"></td><td className="fat">-</td><td className="fat">250 g</td><td className="fat">+</td><td>Jordärtskocka</td></tr>
 
       </tbody>
     </table>
 
    <form className="choose">
-     <input type="text" value="Lägg till artikel" className="add-article" />
+     <input type="text" readOnly value="Lägg till artikel" className="add-article" />
      <select name="count-persons" className="count-shoppinglist">
-          <option value="4">Antal</option>
-          <option value="6">6</option>
-          <option value="8">8</option>
+          <option readOnly value="4">Antal</option>
+          <option readOnly value="6">6</option>
+          <option readOnly value="8">8</option>
       </select>
       <button className="btn-middle">Spara</button>
    </form>
